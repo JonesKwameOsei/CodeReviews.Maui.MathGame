002969ba4575d63609d79335173d270d796c668c
JonesKwameOsei <oseikwamejones@gmail.com>
2025-08-04 06:52:09 +0100

    Submitting MAUI Math Game project for review

---
+++ MauiMathGameApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiMathGameApp;
+
+public partial class MainPage : ContentPage
+{
+  public MainPage()
+  {
+    InitializeComponent();
+  }
+
+  private void OnGameChosen(object sender, EventArgs e)
+  {
+    if (e is TappedEventArgs tappedEventArgs && tappedEventArgs.Parameter is string selectedGameType)
+    {
+      Navigation.PushAsync(new GameSetUpPage(selectedGameType));
+    }
+  }
+
+  private void OnViewPreviousGamesChosen(object sender, EventArgs e)
+  {
+    Navigation.PushAsync(new PreviousGames());
+  }
+}
